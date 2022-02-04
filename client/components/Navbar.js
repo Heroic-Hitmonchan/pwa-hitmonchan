dfd0f5acbbd9d670f7dfb96d4587e507e5429348
Rafet Abdalgalil <rafetalani@gmail.com>
2022-02-03 17:31:33 -0700

    deploy to heroku

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,47 +1,40 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import React from 'react';
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>FS-App-Template</h1>
-    <nav>
-      {isLoggedIn ? (
+const Navbar = () => {
+
+  // const isLoggedIn = useSelector((state) => {
+  //   return !!state.auth.id
+  // })
+
+  return (
+
+
+    <div>
+      <h1>moments</h1>
+      <nav>
+      <input type="file" accept="image/x-png,image/jpeg,image/gif"/>
+        {/* {isLoggedIn ? ( */}
         <div>
           {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
+          {/* <Link to="/home">Home</Link> */}
+          {/* <a href="#" onClick={handleClick}>
             Logout
-          </a>
+          </a> */}
         </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-)
-
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    isLoggedIn: !!state.auth.id
-  }
+        {/* ) : ( */}
+        {/* <div> */}
+        {/* The navbar will show these links before you log in */}
+        {/* <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link> */}
+        {/* </div> */}
+        {/* )} */}
+      </nav>
+      <hr />
+    </div>
+  )
 }
 
-const mapDispatch = dispatch => {
-  return {
-    handleClick() {
-      dispatch(logout())
-    }
-  }
-}
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default Navbar;
