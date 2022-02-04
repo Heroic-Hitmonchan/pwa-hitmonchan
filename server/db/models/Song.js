704ccf0c4785b6a0c8ae83162485f883fcfd53ae
Rafet Abdalgalil <rafetalani@gmail.com>
2022-02-03 20:04:20 -0700

    first push

---
+++ server/db/models/Song.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Song = db.define('song', {
+    spotifySongId: Sequelize.STRING,
+    artistName: Sequelize.STRING,
+    artistSpotifyId: Sequelize.STRING,
+    album: Sequelize.STRING
+})
+
+module.exports = Song
