a8e36fb91864b716edae1db877de654516a3e35e
Rafet Abdalgalil <rafetalani@gmail.com>
2022-02-06 19:04:34 -0700

    added refresh-token hook and api

--- client/store/token.js
+++ client/store/token.js
@@ -1,11 +1,20 @@
 import axios from 'axios'
 
 const SET_TOKEN = 'SET_TOKEN';
+const UPDATE_TOKEN = 'UPDATE_TOKEN'
 
 export const setToken = (token) => {
     return {
         type: SET_TOKEN,
         token
+    }
+}
+
+export const updateToken = (accessToken, expiresIn) => {
+    return {
+        type: UPDATE_TOKEN,
+        accessToken,
+        expiresIn
     }
 }
 
@@ -15,6 +24,8 @@ export default function (state = initialState, action) {
     switch (action.type) {
         case SET_TOKEN:
             return action.token
+        case UPDATE_TOKEN:
+            return { ...state, accessToken: action.accessToken, expiresIn: action.expiresIn }
         default:
             return state
     }
