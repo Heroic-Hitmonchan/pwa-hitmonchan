9fb645a2b2e3923f3f51e755e10b5222baed952e
Kishn Parbadia <kishn.parbadia@gmail.com>
2022-02-10 18:01:14 -0500

    added styling

--- client/components/postUpload/Postupload.js
+++ client/components/postUpload/Postupload.js
@@ -1,77 +1,78 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
-import { addfile, captureNewPhoto } from '../../store/camera'
-import axios from 'axios'
-import './postupload.css'
+import { addfile, captureNewPhoto } from "../../store/camera";
+import axios from "axios";
+import "./postupload.css";
 
 function postUpload() {
+  const [capturedImage, setCapturedImage] = useState("");
 
-  const [capturedImage, setCapturedImage] = useState('')
-
-  const dispatch = useDispatch()
-  let history = useHistory()
+  const dispatch = useDispatch();
+  let history = useHistory();
 
   const { file, photo, token, user, song } = useSelector((state) => {
-
     return {
       photo: state.photo,
       file: state.file,
       token: state.token,
       user: state.user,
-      song: state.song
-    }
+      song: state.song,
+    };
   });
 
   // let capturedImage;
-
 
   useEffect(() => {
     // if ( Object.keys(file).length === 0) {
     if (!file) {
       // console.log(typeof(file))
-      console.log(Object.keys(file).length)
-      history.push('./home')
+      console.log(Object.keys(file).length);
+      history.push("./home");
     } else {
-      setCapturedImage(URL.createObjectURL(file))
+      setCapturedImage(URL.createObjectURL(file));
     }
-  }, [])
-
-
+  }, []);
 
   const matchSong = async () => {
     //get secure URL from server
-    const { data: url } = await axios.get("api/s3Url")
+    const { data: url } = await axios.get("api/s3Url");
     //post the image directly to the s3 bucket
     const headers = {
-      "Content-Type": "multipart/form-data"
-    }
-    await axios.put(url, file, { headers })
+      "Content-Type": "multipart/form-data",
+    };
+    await axios.put(url, file, { headers });
 
-    const imageUrl = url.split("?")[0]
+    const imageUrl = url.split("?")[0];
 
     window.localStorage.setItem("image", JSON.stringify(imageUrl));
 
     dispatch(captureNewPhoto(imageUrl));
-  }
+  };
 
   const anotherImage = () => {
-    dispatch(addfile({}))
-  }
+    dispatch(addfile({}));
+  };
 
   return (
     <div className="post-upload-page-div">
       <div className="post-upload-logo-div">
-        <Link to="/home" onClick={anotherImage} className='post-upload-logo-div-link'>
+        <Link
+          to="/home"
+          onClick={anotherImage}
+          className="post-upload-logo-div-link"
+        >
           <img src="/logo.png" className="post-upload-links-logo" />
         </Link>
       </div>
-      <img
-        src={capturedImage}
-      />
+      <img src={capturedImage} />
       <div className="post-upload-btns-div">
-        <Link to="/songgeneration" onClick={matchSong} className="post-upload-links">
+        <Link
+          to="/songgeneration"
+          onClick={matchSong}
+          className="post-upload-links"
+        >
           Generate song
         </Link>
         <Link to="/home" onClick={anotherImage} className="post-upload-links">
